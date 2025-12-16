5b2f08d56aba6d8d94da540b7d4c98525ea08d74
Yuliia <semockoula@gmail.com>
2025-12-16 09:26:40 +0200

    add functions for categories and products render

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  modal: document.querySelector('.modal'),
+  modalCloseBtn: document.querySelector('.modal__close-btn'),
+  productsList: document.querySelector('.products'),
+  categoriesList: document.querySelector('.categories'),
+};
---
+++ src/js/render-function.js
@@ -0,0 +1,32 @@
+import { refs } from './refs';
+
+export function renderCategories(arrOfCategories) {
+  const arrOfCategoriesAll = ['All', ...arrOfCategories];
+  const markup = arrOfCategoriesAll
+    .map(
+      category => `
+    <li class="categories__item">
+         <button class="categories__btn" type="button">${category}</button>
+    </li>`
+    )
+    .join('');
+
+  refs.categoriesList.insertAdjacentHTML('beforeend', markup);
+}
+
+export function renderProducts(arrOfProducts) {
+  const markup = arrOfProducts
+    .map(
+      ({ id, thumbnail, title, brand, category, price }) => `
+    <li class="products__item" data-id="${id}">
+        <img class="products__image" src="${thumbnail}" alt="${title}"/>
+        <p class="products__title">${title}</p>
+        <p class="products__brand"><span class="products__brand--bold">Brand: ${brand}</span></p>
+        <p class="products__category">Category: ${category}</p>
+        <p class="products__price">Price: ${price}$</p>
+    </li>`
+    )
+    .join('');
+
+  refs.productsList.insertAdjacentHTML('beforeend', markup);
+}
