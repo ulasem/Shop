08b6318554388bdba4d30b23ee3ecba09251b304
ulasem <semockoula@gmail.com>
2026-03-15 16:05:02 +0000

    Deploying to gh-pages from @ ulasem/Shop@980c1499d00222498ca384b42464ecaccc05afef 🚀

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-import"./assets/styles-JE8YjOlG.js";import{a}from"./assets/vendor-2s9xPmg-.js";const r={modal:document.querySelector(".modal"),modalCloseBtn:document.querySelector(".modal__close-btn"),productsList:document.querySelector(".products"),categoriesList:document.querySelector(".categories"),loadMoreBtn:document.querySelector(".load-more-btn"),notFound:document.querySelector(".not-found")};function y(t){const o=["All",...t].map(s=>`
+import"./assets/styles-JE8YjOlG.js";import{a}from"./assets/vendor-2s9xPmg-.js";const c={modal:document.querySelector(".modal"),modalCloseBtn:document.querySelector(".modal__close-btn"),modalProduct:document.querySelector(".modal-product"),productsList:document.querySelector(".products"),categoriesList:document.querySelector(".categories"),loadMoreBtn:document.querySelector(".load-more-btn"),notFound:document.querySelector(".not-found")};function P(t){const e=["All",...t].map(r=>`
     <li class="categories__item">
-         <button class="categories__btn" type="button">${s}</button>
-    </li>`).join("");return r.categoriesList.innerHTML=o}function n(t){const e=t.map(({id:o,thumbnail:s,title:u,brand:g,category:_,price:m})=>`
-    <li class="products__item" data-id="${o}">
-        <img class="products__image" src="${s}" alt="${u}"/>
-        <p class="products__title">${u}</p>
-        <p class="products__brand"><span class="products__brand--bold">Brand: ${g}</span></p>
-        <p class="products__category">Category: ${_}</p>
-        <p class="products__price">Price: ${m}$</p>
-    </li>`).join("");return r.productsList.insertAdjacentHTML("beforeend",e)}const f="https://dummyjson.com",i={CATEGORIES:"/products/category-list",PRODUCTS:"/products",PRODUCTS_BY_ID:"/products/",PRODUCTS_BY_CATEGORY:"/products/category/",SEARCH:"/products/search"};a.defaults.baseURL=f;async function L(){return(await a.get(i.CATEGORIES)).data}async function l(t){const e={limit:12,skip:(t-1)*12};return(await a.get(i.PRODUCTS,{params:e})).data}async function C(t,e){const o={limit:12,skip:(e-1)*12};return(await a.get(`${i.PRODUCTS_BY_CATEGORY}${t}`,{params:o})).data}async function p(t,e){return e==="All"?await l(t):await C(e,t)}function d(t,e){e.currentPage*12>=t.total?r.loadMoreBtn.classList.add("is-hidden"):r.loadMoreBtn.classList.remove("is-hidden")}async function P(t){const e=await p(t.currentPage,t.category);console.log("handleloadMore",e),d(e,t),n(e.products)}async function b(t,e){if(!t.target.classList.contains("categories__btn"))return;document.querySelectorAll(".categories__btn").forEach(s=>s.classList.remove("categories__btn--active")),t.target.classList.add("categories__btn--active"),e.category=t.target.textContent,e.currentPage=1,r.productsList.innerHTML="";const o=await p(e.currentPage,e.category);o.products.length===0?r.notFound.classList.add("not-found--visible"):r.notFound.classList.remove("not-found--visible"),d(o,e),n(o.products)}const c={currentPage:1,category:"All"};S();A();r.loadMoreBtn.addEventListener("click",()=>{c.currentPage+=1,P(c)});r.categoriesList.addEventListener("click",t=>b(t,c));async function S(){const t=await L();y(t)}async function A(){const t=await l(c.currentPage);d(t,c),n(t.products)}
+         <button class="categories__btn" type="button">${r}</button>
+    </li>`).join("");return c.categoriesList.innerHTML=e}function p(t){const o=t.map(({id:e,thumbnail:r,title:n,brand:i,category:l,price:u})=>`
+    <li class="products__item" data-id="${e}">
+        <img class="products__image" src="${r}" alt="${n}"/>
+        <p class="products__title">${n}</p>
+        <p class="products__brand"><span class="products__brand--bold">Brand: ${i}</span></p>
+        <p class="products__category">Category: ${l}</p>
+        <p class="products__price">Price: ${u}$</p>
+    </li>`).join("");return c.productsList.insertAdjacentHTML("beforeend",o)}function L({id:t,thumbnail:o,title:e,tags:r,price:n,description:i,shippingInformation:l,returnPolicy:u}){const f=`<li data-id="${t}">
+      <img class="modal-product__img" src="${o}" alt="${e}" />
+      <div class="modal-product__content">
+        <p class="modal-product__title">${e}</p>
+        <ul class="modal-product__tags">${r.join(", ")}</ul>
+        <p class="modal-product__description">${i}</p>
+        <p class="modal-product__shipping-information">Shipping: ${l}</p>
+        <p class="modal-product__return-policy">Return Policy: ${u}</p>
+        <p class="modal-product__price">Price: ${n} $</p>
+        <button class="modal-product__buy-btn" type="button">Buy</button>
+      </div>
+    </li>`;return c.modalProduct.innerHTML=f}const C="https://dummyjson.com",d={CATEGORIES:"/products/category-list",PRODUCTS:"/products",PRODUCTS_BY_ID:"/products/",PRODUCTS_BY_CATEGORY:"/products/category/",SEARCH:"/products/search"};a.defaults.baseURL=C;async function $(){return(await a.get(d.CATEGORIES)).data}async function _(t){const o={limit:12,skip:(t-1)*12};return(await a.get(d.PRODUCTS,{params:o})).data}async function b(t){return(await a.get(`${d.PRODUCTS_BY_ID}${t}`)).data}async function S(t,o){const e={limit:12,skip:(o-1)*12};return(await a.get(`${d.PRODUCTS_BY_CATEGORY}${t}`,{params:e})).data}async function m(t,o){return o==="All"?await _(t):await S(o,t)}function g(t,o){o.currentPage*12>=t.total?c.loadMoreBtn.classList.add("is-hidden"):c.loadMoreBtn.classList.remove("is-hidden")}function y(){c.modal.classList.toggle("modal--is-open")}async function B(t){const o=await m(t.currentPage,t.category);console.log("handleloadMore",o),g(o,t),p(o.products)}async function A(t,o){if(!t.target.classList.contains("categories__btn"))return;document.querySelectorAll(".categories__btn").forEach(r=>r.classList.remove("categories__btn--active")),t.target.classList.add("categories__btn--active"),o.category=t.target.textContent,o.currentPage=1,c.productsList.innerHTML="";const e=await m(o.currentPage,o.category);e.products.length===0?c.notFound.classList.add("not-found--visible"):c.notFound.classList.remove("not-found--visible"),g(e,o),p(e.products)}async function T(t){const o=t.target.closest(".products__item");if(!o)return;const e=o.dataset.id,r=await b(e);L(r),y()}const s={currentPage:1,category:"All"};h();E();c.loadMoreBtn.addEventListener("click",()=>{s.currentPage+=1,B(s)});c.categoriesList.addEventListener("click",t=>A(t,s));c.productsList.addEventListener("click",T);c.modalCloseBtn.addEventListener("click",y);async function h(){const t=await $();P(t)}async function E(){const t=await _(s.currentPage);g(t,s),p(t.products)}
 //# sourceMappingURL=index.js.map
